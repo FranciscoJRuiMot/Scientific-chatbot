d74c9358ed6fabae89d80b59b53d216b25c36158
FranciscoJRuiMot <pacopeperuiz.mota@gmail.com>
2024-06-21 22:10:54 +0200

    Update question_function.py

--- question_function.py
+++ question_function.py
@@ -5,10 +5,10 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 
 def keys_from_question():
-    question = input()
-    translator = GoogleTranslator(source = 'es', target = 'en')
-    tokens = word_tokenize(translator.translate(question))
-    stop_words = stopwords.words('english')
+    question = str(input())
+    tokens = word_tokenize(question)
+    stop_words = stopwords.words('spanish')
     words = [word for word in tokens if word.isalpha()]
     key_words = [key for key in words if not key in stop_words]
+    #to do: se lanza una interfaz al usuario con estas key_worlds para que las seleccione
     return key_words
